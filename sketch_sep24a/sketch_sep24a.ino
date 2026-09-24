44ea9a2dd16451c296727ab8c11d9c6e6964a73f
Shahriyor Muhidinov <shahriyor02122012@gmail.com>
2026-09-24 19:36:52 +0500

    Connected the dino to the arduion board

---
+++ sketch_sep24a/sketch_sep24a.ino
@@ -0,0 +1,20 @@
+const int BTN_LEFT = 3;
+const int BTN_RIGHT = 2;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(BTN_LEFT, INPUT_PULLUP);
+  pinMode(BTN_RIGHT, INPUT_PULLUP);
+}
+
+void loop() {
+  // Read state (LOW = pressed due to INPUT_PULLUP)
+  int leftState = digitalRead(BTN_LEFT) == LOW ? 1 : 0;
+  int rightState = digitalRead(BTN_RIGHT) == LOW ? 1 : 0;
+
+  // Send formatted string: "1,0\n"
+  Serial.print(leftState);
+  Serial.print(",");
+  Serial.println(rightState);
+
+}
